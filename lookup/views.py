505fd06d9c317f9d7db8bd6280ca102d66b6f7dd
Raghuveer Chimata <chmt.raghuveer@gmail.com>
2020-05-14 01:58:30 +0200

    added api url in views.py

--- lookup/views.py
+++ lookup/views.py
@@ -5,7 +5,18 @@ from django.shortcuts import render
 # this is my views.py
 
 def home(request):
-	return render(request,'home.html',{})
+
+	import json
+	import requests
+
+	api_requests= requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=E48705BF-EE6B-408D-945E-89BCC0B8E69B")
+
+	try:
+		api = json.loads(api_requests.content)
+	except Exception as e:
+		api = "Error ..."
+
+	return render(request,'home.html',{ 'api' : api})
 
 
 def about(request):
